699a1e15ca558dd548632a729b7d4c5dc72c60e2
Ginno Taimal <ginno.taimal@epn.edu.ec>
2023-06-30 07:46:07 -0500

    feat: Add general menu (UI)

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 
+#include "Doctor/doctor.h"
+#include "Paciente/paciente.h"
+#include "Valoracion/valoracion.h"
+
 int main() {
-    printf("Hello, World!\n");
+    int opcion;
+    do {
+        printf("-- Menu Principal --\n");
+        printf("1. Paciente\n");
+        printf("2. Doctor\n");
+        printf("3. Valoracion y Comentarios\n");
+        printf("4. Salir\n");
+        printf("Seleccione una opcion: ");
+        scanf("%i", &opcion);
+
+        switch (opcion) {
+            case 1:
+                pacienteMenu();
+                break;
+            case 2:
+                doctorMenu();
+                break;
+            case 3:
+                valoracionMenu();
+                break;
+            case 4:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opcion invalida. Intente nuevamente.\n");
+                break;
+        }
+    } while (opcion != 4);
+
     return 0;
 }
