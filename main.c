0e719dff6091d003dbbe9b649e92420357d4d3ba
Ginno Taimal <ginno.taimal@epn.edu.ec>
2023-07-02 07:14:40 -0500

    feat: Add the condition for the data agreement

--- main.c
+++ main.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "Doctor/doctor.h"
 #include "Paciente/paciente.h"
 #include "Valoracion/valoracion.h"
+#include "Acuerdo/acuerdo.h"
 
 int main() {
-    int opcion;
-    do {
-        printf("-- Menu Principal --\n");
-        printf("1. Paciente\n");
-        printf("2. Doctor\n");
-        printf("3. Valoracion y Comentarios\n");
-        printf("4. Salir\n");
-        printf("Seleccione una opcion: ");
-        scanf("%i", &opcion);
+    if (acuerdo() == 1) {
+        system("cls");
+        int opcion;
+        do {
+            printf("-- Menu Principal --\n");
+            printf("1. Paciente\n");
+            printf("2. Doctor\n");
+            printf("3. Valoracion y Comentarios\n");
+            printf("4. Salir\n");
+            printf("Seleccione una opcion: ");
+            scanf("%i", &opcion);
 
-        switch (opcion) {
-            case 1:
-                pacienteMenu();
-                break;
-            case 2:
-                doctorMenu();
-                break;
-            case 3:
-                valoracionMenu();
-                break;
-            case 4:
-                printf("Saliendo del programa...\n");
-                break;
-            default:
-                printf("Opcion invalida. Intente nuevamente.\n");
-                break;
-        }
-    } while (opcion != 4);
-
+            switch (opcion) {
+                case 1:
+                    pacienteMenu();
+                    break;
+                case 2:
+                    doctorMenu();
+                    break;
+                case 3:
+                    valoracionMenu();
+                    break;
+                case 4:
+                    printf("Saliendo del programa...\n");
+                    break;
+                default:
+                    printf("Opcion invalida. Intente nuevamente.\n");
+                    break;
+            }
+        } while (opcion != 4);
+    }
     return 0;
 }
